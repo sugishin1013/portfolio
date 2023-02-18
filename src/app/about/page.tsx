70032d5d295f549794ab43dcfb4fc1be510b1574
sugishin1013 <shin10132022@gmail.com>
2023-02-18 15:41:56 +0900

    update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,3 @@
-import NextIcon from '../nextIcon'
-import ReactIcon from '../reactIcon'
-import TailwindIcon from '../tailwindIcon'
-import TypeScriptIcon from '../typeScriptIcon'
-
 export default function About() {
   return (
     <main className='w-screen h-screen flex justify-center lg:items-center lg:pt-0 pt-14 px-4'>
@@ -10,23 +5,7 @@ export default function About() {
         <h2 className='lg:text-8xl text-4xl text-center text-slate-700 dark:text-white'>About</h2>
         <p className='lg:my-10 my-5 lg:text-xl text-base text-slate-700 dark:text-white leading-relaxed'>
           フロントエンドエンジニア「sugishin1013」のポートフォリオサイトです。これまでに手がけた制作物、身につけたスキルをまとめています。
-          下記のアイコンはこのサイトで使用している技術スタックです。
-          今後も新しいことを学習しつつ、このサイトで色々試して追加していきます。
         </p>
-        <ul className='flex justify-center items-center w-full'>
-          <li className='lg:w-20 lg:h-20 w-10 h-10 lg:mr-10 m-4'>
-            <ReactIcon />
-          </li>
-          <li className='lg:w-20 lg:h-20 w-10 h-10 lg:mr-10 m-4'>
-            <NextIcon />
-          </li>
-          <li className='lg:w-20 lg:h-20 w-10 h-10 lg:mr-10 m-4'>
-            <TypeScriptIcon />
-          </li>
-          <li className='lg:w-20 lg:h-20 w-10 h-10 lg:mr-10 m-4'>
-            <TailwindIcon />
-          </li>
-        </ul>
       </div>
     </main>
   )
